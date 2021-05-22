0c4580ddd6699d64c9a3c7127c4886278c68189a
Yaseen <mhdyaz@outlook.com>
2021-05-22 23:23:57 +0300

    Profile function in header

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -2,6 +2,40 @@ import React, { Component } from 'react';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import Input from '@material-ui/core/Input';
+import IconButton from '@material-ui/core/IconButton';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Button from '@material-ui/core/Button';
+
+function ProfileMenu() {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <div className="profile-icon">
+            <DeleteIcon onClick={handleClick}/>
+            <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+            >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>Logout</MenuItem>
+            </Menu>
+        </div>
+    );
+}
+
 
 class Header extends Component {
 
@@ -9,7 +43,8 @@ class Header extends Component {
         super();
         this.state = {
             search: "",
-            loggedIn: sessionStorage.getItem("access-token") == null ? false : true
+            loggedIn: sessionStorage.getItem("access-token") == null ? false : true,
+            profilePic: "",
         }
     }
 
@@ -23,9 +58,12 @@ class Header extends Component {
             <div className="header-container">
                 <p className="header-text">Image Viewer</p>
                 { this.state.loggedIn &&
-                    <div className="search-bar">
-                        <SearchIcon />
-                        <Input id="searchbox"  placeholder="Search" type="text" username={this.state.search} onChange={this.inputSearchChangeHandler} />
+                    <div>
+                        <ProfileMenu/>
+                        <div className="search-bar">
+                            <SearchIcon />
+                            <Input id="searchbox" disableUnderline={true} placeholder="Search" type="text" username={this.state.search} onChange={this.inputSearchChangeHandler} />
+                        </div>
                     </div>
                 }
             </div>
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -7,53 +7,55 @@ import Header from '../../common/header/Header';
 const MEDIAID_EP= "https://graph.instagram.com/me/media?fields=id,caption&access_token=YourAccessToken";
 const DETAILS_EP ="https://graph.instagram.com/MediaID?fields=id,media_type,media_url,username,timestamp&access_token=YourAccessToken";
 
+
 class Home extends Component {
     
     constructor() {
         super();
         this.state = {
-            someText:"Am Home!",
-            loggedIn: sessionStorage.getItem("access-token") == null ? false : true
+            searchText:"",
+            authToken: sessionStorage.getItem("access-token"),
+            mediaContent:{}
         }
     }
     
-    componentWillMount() {
-        if (this.state.loggedIn === false ){
+    componentDidMount() {
+        if (this.state.authToken === null ){
             this.props.history.push('/');
+        } else {
+            this.getImages("");
         }
+    }
+
+    getImages=(filter) => {
         let data = null;
         let xhr = new XMLHttpRequest();
         let that = this;
         xhr.addEventListener("readystatechange", function () {
             if (this.readyState === 4) {
+                let allMedia = JSON.parse(this.responseText);
+                let media = allMedia.data.map( (media, index, array)=> {
+                    if (media.caption.toUpperCase().includes(filter.toUpperCase())) {
+                        return media;
+                    } 
+
+                    return null;
+                });
+                console.log(allMedia);
                 that.setState({
-                    upcomingMovies: JSON.parse(this.responseText).movies
+                    mediaContent : allMedia
                 });
             }
         });
-
-        xhr.open("GET", this.props.baseUrl + "movies?status=PUBLISHED");
+        var url = MEDIAID_EP.replace(/YourAccessToken/g,this.state.authToken);
+        xhr.open("GET", url);
         xhr.setRequestHeader("Cache-Control", "no-cache");
         xhr.send(data);
 
-
-        let dataReleased = null;
-        let xhrReleased = new XMLHttpRequest();
-        xhrReleased.addEventListener("readystatechange", function () {
-            if (this.readyState === 4) {
-                that.setState({
-                    releasedMovies: JSON.parse(this.responseText).movies
-                });
-            }
-        });
-
-        xhrReleased.open("GET", this.props.baseUrl + "movies?status=RELEASED");
-        xhrReleased.setRequestHeader("Cache-Control", "no-cache");
-        xhrReleased.send(dataReleased);
     }
 
     searchHandler = (text) => {
-        alert (text);
+        this.getImages(text);
     }
 
     render() {
