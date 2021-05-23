acbce5972d7c64fc71172cc871c42fd175c725c1
Yaseen <mhdyaz@outlook.com>
2021-05-24 00:04:02 +0300

    Added mock data as API not working

---
+++ src/assets/data/Data.js
@@ -0,0 +1,55 @@
+export var media = {
+    "data": [
+        {
+            "id": "17895695668004550",
+            "caption": "Join UpGrad’s got talent! Enrol yourself in a program and upgrade your skills!"
+        },
+        {
+            "id": "17899305451014820",
+            "caption": "image1"
+        },
+        {
+            "id": "17896450804038745",
+            "caption": "image2"
+        },
+        {
+            "id": "17881042411086627",
+            "caption": "image3"
+        }
+    ],
+    "profile_picture": 'https://raw.githubusercontent.com/imnoor/image-viewer/master/src/assets/profile.png'
+};
+
+export var images = {
+    "17895695668004550": {
+        "id": "17895695668004550",
+        "media_type": "IMAGE",
+        "media_url": "https://fb-s-b-a.akamaihd.net/...",
+        "username": "upgrad_sd",
+        "timestamp": "2017-08-31T18:10:00+0000"
+    },
+    "17899305451014820":
+    {
+        "id": "17899305451014820",
+        "media_type": "IMAGE",
+        "media_url": "https://fb-s-b-a.akamaihd.net/...",
+        "username": "upgrad_sd",
+        "timestamp": "2018-08-31T18:10:00+0000"
+    },
+    "17896450804038745":
+    {
+        "id": "17896450804038745",
+        "media_type": "IMAGE",
+        "media_url": "https://fb-s-b-a.akamaihd.net/...",
+        "username": "upgrad_sd",
+        "timestamp": "2019-08-31T18:10:00+0000"
+    },
+    "17881042411086627":
+    {
+        "id": "17881042411086627",
+        "media_type": "IMAGE",
+        "media_url": "https://fb-s-b-a.akamaihd.net/...",
+        "username": "upgrad_sd",
+        "timestamp": "2020-08-31T18:10:00+0000"
+    }
+  };
