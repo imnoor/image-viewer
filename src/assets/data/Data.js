472559e87c7447e2ede3d6c7f76638a138b701eb
Yaseen <mhdyaz@outlook.com>
2021-05-24 00:08:00 +0300

    Updated sample data

--- src/assets/data/Data.js
+++ src/assets/data/Data.js
@@ -24,7 +24,7 @@ export var images = {
     "17895695668004550": {
         "id": "17895695668004550",
         "media_type": "IMAGE",
-        "media_url": "https://fb-s-b-a.akamaihd.net/...",
+        "media_url": "http://images5.fanpop.com/image/photos/28000000/randomised-random-28065165-1024-819.jpg",
         "username": "upgrad_sd",
         "timestamp": "2017-08-31T18:10:00+0000"
     },
@@ -32,7 +32,7 @@ export var images = {
     {
         "id": "17899305451014820",
         "media_type": "IMAGE",
-        "media_url": "https://fb-s-b-a.akamaihd.net/...",
+        "media_url": "http://bluefaqs.com/wp-content/uploads/2010/06/Random-Images-Thumb.jpg",
         "username": "upgrad_sd",
         "timestamp": "2018-08-31T18:10:00+0000"
     },
@@ -40,7 +40,7 @@ export var images = {
     {
         "id": "17896450804038745",
         "media_type": "IMAGE",
-        "media_url": "https://fb-s-b-a.akamaihd.net/...",
+        "media_url": "http://images2.fanpop.com/image/photos/9400000/Random-random-9449476-1680-1050.jpg",
         "username": "upgrad_sd",
         "timestamp": "2019-08-31T18:10:00+0000"
     },
@@ -48,7 +48,7 @@ export var images = {
     {
         "id": "17881042411086627",
         "media_type": "IMAGE",
-        "media_url": "https://fb-s-b-a.akamaihd.net/...",
+        "media_url": "https://3.bp.blogspot.com/-8YfDSThUslI/T7j2aFQfBAI/AAAAAAAADSU/jx23qdF9jC4/s1600/rendom_cuteness_picture_12.jpg",
         "username": "upgrad_sd",
         "timestamp": "2020-08-31T18:10:00+0000"
     }
